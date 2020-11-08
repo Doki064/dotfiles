10a5ecc1c618c8d716d111ad4223dbff580d3ba3
Doki064 <52833936+Doki064@users.noreply.github.com>
2020-11-08 08:13:39 +0700

    Fix install.sh

--- install.sh
+++ install.sh
@@ -8,14 +8,14 @@ for f in $(find config/* -type f); do
         mkdir -p "$HOME/.$(dirname $f)"
     fi
     
-    if [[ -f "$HOME/.$f" ]]; then
-        mv "$HOME/.$f" "$HOME/.$f.bak"
-    fi
+#    if [[ -f "$HOME/.$f" ]]; then
+#        mv "$HOME/.$f" "$HOME/.$f.bak"
+#    fi
     
-    ln -sf "$(readlink -f $f)" "$HOME/.$f"
+    ln -sfn "$(readlink -f $f)" "$HOME/.$f"
 done
 
 # Install personal zsh config
-ln -sf "$(readlink -f ./zsh_files)" "$HOME/.zsh_files" && \
-ln -sf "$(readlink -f ./zshrc)" "$HOME/.zshrc" && \
-ln -sf "$(readlink -f ./p10k.zsh)" "$HOME/.p10k.zsh"
+ln -sfn "$(readlink -f ./zsh_files)" "$HOME/.zsh_files" && \
+ln -sfn "$(readlink -f ./zshrc)" "$HOME/.zshrc" && \
+ln -sfn "$(readlink -f ./p10k.zsh)" "$HOME/.p10k.zsh"
